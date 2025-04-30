0ea6176fffda02df9a56df15ba737d4688b779ad
raheeljafarani <rjafaran@chicagobooth.edu>
2025-04-30 00:31:51 +0000

    added openAI formatting and model and system

---
+++ app/controllers/macros_controller.rb
@@ -0,0 +1,58 @@
+class MacrosController < ApplicationController
+
+  def display_form
+    render({ :template => "macros_templates/new_form"})
+  end 
+
+  def do_magic
+    @the_description = params.fetch("description_param")
+    @the_image = params.fetch("image_param")
+    @the_image_converted = DataURI.convert(@the_image)
+
+    c = OpenAI::Chat.new
+    c.model = "gpt-4.1-nano"
+    c.system("You are an expert nutritionist. Your job is to estimate how many grams of carbohydrates, grams of protein, grams of fat, and total calories are in a meal. You should also add a breakdown of how you arrived at these figures, and any other notes you have. The user will provide either a photo, a description, or both.")
+    c.schema = '{
+    "name": "nutrition_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "number",
+            "description": "Amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "number",
+            "description": "Amount of protein in grams."
+          },
+          "fat": {
+            "type": "number",
+            "description": "Amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "number",
+            "description": "Total calories in kcal."
+          },
+          "notes": {
+            "type": "string",
+            "description": "A breakdown of how you arrived at the values, and additional notes."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories",
+          "notes"
+        ],
+        "additionalProperties": false
+      },
+      "strict": true
+    }'
+    
+    c.user(@the_description, image: @the_image)
+    @result = c.assistant!
+
+    render({ :template => "macros_templates/results"})
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+get("/blank_form", { :controller => "macros", :action => "display_form"})
+
+post("/process_input", { :controller => "macros", :action => "do_magic"})
 
   # Solutions below. Don't peek until you try it yourself and get stuck!
 
